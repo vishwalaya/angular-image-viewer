ee3e79b21ba0b6a29e99909fec62d5f7d9212671
vishwalaya <vishwa@vishwalaya.net>
2023-12-10 13:21:42 +0530

    Tes

--- projects/angular-image-viewer/index.ts
+++ projects/angular-image-viewer/index.ts
@@ -4,3 +4,4 @@ export * from './src/lib//angular-image-viewer.module';
 export { ImageViewerConfig} from './src/lib//models/image-viewer-config.model';
 export { CustomImageEvent } from './src/lib//models/custom-image-event-model';
 export { FullScreenDirective } from './src/lib/directives/full-screen.directive';
+export { AngularImageViewerModule } from './src/lib//angular-image-viewer.module';
